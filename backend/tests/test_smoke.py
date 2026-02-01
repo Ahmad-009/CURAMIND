21adc64b2b35a76154496d6014383607e2249105
Ahmad-009 <manj2825@gmail.com>
2026-02-01 15:40:53 +0500

    fix(smoketest):replace requests with testClient [CURA-11]

--- backend/tests/test_smoke.py
+++ backend/tests/test_smoke.py
@@ -3,7 +3,8 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_health_check():
-    response = client.get("/") 
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK!!!"}
